e93c0e5778bb5f5e6e0ed923b88170b12f3f0ab7
Udit Kumar Tiwari <159471583+Udit004@users.noreply.github.com>
2025-12-10 22:58:29 +0530

    prsima setup.

---
+++ prisma/migrations/20251210064807_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Reminder` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `body` VARCHAR(191) NULL,
+    `remindAt` DATETIME(3) NOT NULL,
+    `status` ENUM('PENDING', 'COMPLETED', 'CANCELLED') NOT NULL DEFAULT 'PENDING',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
