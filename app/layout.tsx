95613410ff3dd4fdca8bb6d4f82a97af50fa9d87
Udit Kumar Tiwari <159471583+Udit004@users.noreply.github.com>
2025-12-14 14:28:34 +0530

    icon change.

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>⏰</text></svg>" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
         <QueryProvider>{children}</QueryProvider>
